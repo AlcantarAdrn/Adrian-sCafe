ba08e8f8e55be44da24573da0862a48ebe459ef9
AlcantarAdrn <adriian.alcantar@gmail.com>
2020-12-29 22:34:42 +0000

    index added

--- app.js
+++ app.js
@@ -13,6 +13,28 @@ router.use(express.static(path.resolve(__dirname, 'views')));
 router.use(express.urlencoded({extended:true}));
 router.use(express.json());
 
+function xmlFileToJs(filename, cb){
+var filepath = path.normalize(path.join(__dirname,filename));
+fs.readFile(filepath, 'utf8', function(err,xmlStr) {
+    if(err) throw(err);
+    xml2js.parseString(xmlStr, {}, cb);
+
+});
+}
+
+function jsToXmlFile(filename, obj,cb){
+    var filepath = path.normalize(path.join(__dirname, filename));
+    var builder = new xml2js.Builder();
+    var xml = builder.buildObject(obj);
+    fs.unlinkSync(filepath);
+    fs.writeFile(filepath, xml, cb);
+
+}
+
+router.get('/', function(req,res) {
+    res.render('index');
+
+});
 
 router.get('/get/html', function(req,res) {
 
